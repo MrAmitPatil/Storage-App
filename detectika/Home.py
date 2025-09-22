3aedf8a4e1411778c85b30e275ad31d4a96130de
MrAmitPatil <amit.patil@nusummit.com>
2025-09-22 18:29:46 +0530

    Create Home.py

---
+++ detectika/Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.title('Documentation📖')
+st.subheader('DETECTIKA DOC🔍')
+st.subheader('SENTILYZER DOC😄')
+st.subheader('CHURNWISE DOC📊')
+st.subheader('EQUITY STOCK CASH MARKET DOC💵')
+st.subheader('EQUITY STOCK FUTURES & OPIONS DOC📜')
+st.subheader('INVESTINTEL DOC📋')
